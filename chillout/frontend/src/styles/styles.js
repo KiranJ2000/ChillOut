30d2ef4b803a2c8ac2ca0ff6932dd503026ad102
Kiran <kiranjanardhanan2000@gmail.com>
2021-05-30 21:59:18 +0530

    completed the entry page

---
+++ chillout/frontend/src/styles/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  heading: {
+    backgroundColor: "rgb(242, 242, 242)",
+  },
+}));
+
+export default useStyles;
